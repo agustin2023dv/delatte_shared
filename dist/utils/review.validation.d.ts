f708c21fb78545ed9f56bf1fe37ff6b8b8069b55
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-02-22 20:41:38 +1100

    exportacion de utils

--- dist/utils/review.validation.d.ts
+++ dist/utils/review.validation.d.ts
@@ -1,3 +1,3 @@
-export declare const validateReview: any[];
-export declare const validateReviewUpdate: any[];
+export declare const validateReview: import("express-validator").ValidationChain[];
+export declare const validateReviewUpdate: import("express-validator").ValidationChain[];
 //# sourceMappingURL=review.validation.d.ts.map

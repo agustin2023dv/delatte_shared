c6af2e1b56ca5496e6fd5a3f696d36e2f8f4f30a
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-03-02 15:49:14 +0900

    cambio en IReviews p traer los campos pedidos con populate

--- interfaces/IReview.ts
+++ interfaces/IReview.ts
@@ -1,10 +1,8 @@
 import { Document, ObjectId } from 'mongoose';
-import { IUser } from './IUser.js';
-import { IRestaurant } from './IRestaurant.js';
 
 export interface IReview extends Document {
     _id: ObjectId; // Identificador único de la reseña
-    restaurante: ObjectId ; // Relación con el restaurante reseñado
+    restaurante: ObjectId  | { _id: ObjectId | string; nombre: string ; direccion: string  }; // Relación con el restaurante reseñado
     usuario: ObjectId ; // Relación con el usuario que hizo la reseña
     calificacion: number; // Calificación del restaurante, p. ej. del 1 al 5
     comentario: string; // Comentario de la reseña

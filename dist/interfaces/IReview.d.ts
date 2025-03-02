4f938cb6da09b1f3c5e89141979d4b76989e572a
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-03-02 15:49:48 +0900

    habia falta2 el build

--- dist/interfaces/IReview.d.ts
+++ dist/interfaces/IReview.d.ts
@@ -1,7 +1,11 @@
 import { Document, ObjectId } from 'mongoose';
 export interface IReview extends Document {
     _id: ObjectId;
-    restaurante: ObjectId;
+    restaurante: ObjectId | {
+        _id: ObjectId | string;
+        nombre: string;
+        direccion: string;
+    };
     usuario: ObjectId;
     calificacion: number;
     comentario: string;

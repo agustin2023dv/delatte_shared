dc95c86588b3266c1ab5ab9c2ae16272dc848e72
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-03-09 19:41:54 +0900

    IReservation up up

--- dist/interfaces/IReservation.d.ts
+++ dist/interfaces/IReservation.d.ts
@@ -1,23 +1,13 @@
-import { Document, ObjectId } from 'mongoose';
+import { Document, ObjectId } from "mongoose";
 export interface IReservation extends Document {
-    _id: ObjectId | string;
-    restaurante: ObjectId | {
-        _id: ObjectId | string;
-        nombre: string;
-        direccion: string;
-    };
-    usuario: ObjectId | {
-        _id: ObjectId | string;
-        nombre: string;
-        apellido: string;
-        email: string;
-        phone: string;
-    };
+    _id: ObjectId;
+    restaurante: ObjectId;
+    usuario: ObjectId;
     fecha: Date;
     horario: string;
     numAdultos: number;
     numNinos: number;
-    pedidosEspeciales: string;
+    pedidosEspeciales?: string;
     estado: "Pasada" | "Confirmada" | "Cancelada";
     fechaCreacion: Date;
 }
--- interfaces/IReservation.ts
+++ interfaces/IReservation.ts
@@ -1,14 +1,14 @@
-    import { Document, ObjectId } from 'mongoose';
+import { Document, ObjectId } from "mongoose";
 
-    export interface IReservation extends Document {
-      _id: ObjectId | string; // ID de la reserva
-      restaurante: ObjectId | { _id: ObjectId | string; nombre: string ; direccion: string  }; // ID del restaurante
-      usuario: ObjectId | { _id: ObjectId | string; nombre: string ; apellido: string; email: string, phone: string  }; // ID del usuario que realiza la reserva
-      fecha: Date; // Fecha completa de la reserva (día y hora)
-      horario: string; // Horario de la reserva (formato HH:MM)
-      numAdultos: number; // Número de adultos
-      numNinos: number; // Número de niños
-      pedidosEspeciales: string; // Nota opcional del usuario (máx. 500 caracteres)
-      estado: "Pasada" | "Confirmada" | "Cancelada"; // Estado de la reserva
-      fechaCreacion: Date; // Fecha de creación de la reserva
-    }
+export interface IReservation extends Document {
+  _id: ObjectId;
+  restaurante: ObjectId;
+  usuario: ObjectId;
+  fecha: Date;
+  horario: string;
+  numAdultos: number;
+  numNinos: number;
+  pedidosEspeciales?: string;
+  estado: "Pasada" | "Confirmada" | "Cancelada";
+  fechaCreacion: Date;
+}

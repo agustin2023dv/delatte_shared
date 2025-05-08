400e7ada5620052ee573eb97f124a55eb98551e7
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-05-08 07:38:02 -0300

    refactor(dtos/menu): mejora IMenuResponseDTO con IDs y estructura anidada precisa

--- dist/dtos/menus/base/menu-response.dto.d.ts
+++ dist/dtos/menus/base/menu-response.dto.d.ts
@@ -1,15 +1,17 @@
-import { IMenuItemWithId } from "../../../interfaces/Menu/IMenuItemWithId";
+import { IMenuItemWithId } from '../../../interfaces/Menu/IMenuItemWithId';
 /**
  * DTO de salida para retornar un menú completo.
  */
 export interface IMenuResponseDTO {
     _id: string;
+    tipo: 'Comida' | 'Bebidas' | 'Postres';
+    items: IMenuItemWithId[];
     restaurante: {
         _id: string;
         nombre: string;
         direccion: string;
+        managerPrincipal: string;
+        coManagers?: string[];
     };
-    tipo: 'Comida' | 'Bebidas' | 'Postres';
-    items: IMenuItemWithId[];
 }
 //# sourceMappingURL=menu-response.dto.d.ts.map
--- dtos/menus/base/menu-response.dto.ts
+++ dtos/menus/base/menu-response.dto.ts
@@ -1,16 +1,18 @@
-import { IMenuItemWithId } from "../../../interfaces/Menu/IMenuItemWithId";
-
+import { IMenuItemWithId } from '../../../interfaces/Menu/IMenuItemWithId';
 
 /**
  * DTO de salida para retornar un menú completo.
  */
 export interface IMenuResponseDTO {
   _id: string;
+  tipo: 'Comida' | 'Bebidas' | 'Postres';
+  items: IMenuItemWithId[];
+
   restaurante: {
     _id: string;
     nombre: string;
     direccion: string;
+    managerPrincipal: string;
+    coManagers?: string[]; 
   };
-  tipo: 'Comida' | 'Bebidas' | 'Postres';
-  items: IMenuItemWithId[]; 
 }
